530354d711b0145f5a698efddd86c7e858709bfd
Emir Kurtkan <emircan.kurtkan@yahoo.com.tr>
2021-06-07 18:25:57 +0300

    added to repository

---
+++ Account_Expire_Check/AccountExpired-users.ps1
@@ -0,0 +1,23 @@
+﻿Import-Module ActiveDirectory
+
+
+$server=Search-ADAccount -AccountExpired -UsersOnly -Server SERVERNAME.domain.local | select SamAccountName
+if ($server.count -eq $null -and $server -eq $null){
+    $a=0
+}
+Elseif ($server.count -eq $null -and $server -ne $null){
+    $a=1
+}
+Else
+{
+    $a=@($server.count)
+    
+}
+Write-Host "<prtg>"
+Write-Host "<result>" 
+"<channel>Locked Out Users</channel>" 
+    
+"<value>"+ $a +"</value>" 
+"</result>"
+"<text>" + (($server | select SamAccountName | ConvertTo-Csv -NoTypeInformation | select -skip 1 ) -join ", ").replace("""","") + "</text>"
+Write-Host "</prtg>"
---
+++ Default_Computers_OU_Check/default_computers_ou_check.ps1
@@ -0,0 +1,33 @@
+﻿#server info ( Change this with your Active Directory Server )
+$DomainController = SERVERNAME.domain.local
+
+#times
+$Daysactive = 7
+$Daysinactive = 21
+
+# Logged in within 7 days, no "stock" comment in description;
+$activetime = (Get-Date).Adddays(-($Daysactive))
+#Not logged in within 21 days;
+$inactivetime = (Get-Date).Adddays(-($Daysinactive))
+
+
+# Logged in within 7 days, no "stock" comment in description, Devices in "Computers" organizational unit;
+$ActiveComputers = Get-ADComputer -Filter {LastLogonTimeStamp -gt $activetime} -ResultPageSize 2000 -resultSetSize $null -Properties Name, Lastlogondate, Description -Server $DomainController -SearchBase "OU=Computers,DC=domain,DC=local" | where {$_.Description -notlike "*stock*"} | Measure-Object | select -ExpandProperty Count
+#Not logged in within 21 days, Devices in "Computers" organizational unit;
+$InactiveComputers=Get-ADComputer -Filter {LastLogonTimeStamp -lt $inactivetime} -ResultPageSize 2000 -resultSetSize $null -Properties Name, Lastlogondate, Description -Server $DomainController -SearchBase "OU=Computers,DC=domain,DC=local" | where {$_.Description -notlike "*stock*"} | Measure-Object | select -ExpandProperty Count
+
+
+$PRTG="
+<prtg>
+  <result>
+    <channel>Active Computers</channel>
+    <value>$ActiveComputers</value>
+  </result>
+  <result>
+    <channel>Inactive Computers</channel>
+    <value>$InactiveComputers</value>
+  </result>
+</prtg>
+"
+
+$PRTG
